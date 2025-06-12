5235d3d0b22834bbfd6a98d6a654b22a84816c8c
Errorsia <Errorsia@outlook.com>
2025-06-12 19:27:29 +0800

    Version 3.0

--- random-number-generator.py
+++ random-number-generator.py
@@ -3,7 +3,7 @@
 # Author: Errorsia <Errorsia@outlook.com>
 # License: GPL v3
 
-# Version 2.0
+# Version 3.0
 
 import tkinter as tk
 from tkinter import messagebox
@@ -29,102 +29,47 @@ def exceed_len_max(num1, num2):
     return False
 
 
-"""
-def is_number(str1, str2):
 
-    first_character_str1 = str1[0]
-    first_character_str2 = str2[0]
 
-    list_remain_characters = [str1[1:], str2[1:]]
-    digits_in_remain_characters = 0
-    list_first_character = [first_character_str1, first_character_str2]
-    digits_in_first_character = 0
+def is_number(string):
 
-    is_int = False
+    first_character = string[0]
+    remain_characters = string[1:]
 
-    total_length_minus_two = len(str1) + len(str2) - 2
-
-    
-    这边其实有一个讨巧的办法：
-    try:
-        int character_input
-    except:
-        print(character_input)
-    
-    for list_tmp in list_remain_characters:
-        for character_input in list_tmp:
-            for character in "1234567890":
-                if character_input== character:
-                    # print("j, k: %s, %s" % (j, k))
-                    digits_in_remain_characters += 1
-                    break
-
-    for character_input in list_first_character:
-        for character in "1234567890-":
-            if character_input == character:
-                digits_in_first_character += 1
-                break
-
-    # print(total_length_minus_two)
-    if digits_in_remain_characters == total_length_minus_two and digits_in_first_character == 2:
-        is_int = True
-
-    if is_int:
-        return True
-    else:
-        # print("Error: Nor int number:")
+    if first_character == "-" and len(string) == 1:
         tk.messagebox.showerror(title="Error", message="请输入整数!")
         return False
-"""
-
-
-def is_number(str1):
-
-    first_character_str1 = str1[0]
-
-    remain_characters = str1[1:]
-    digits_in_remain_characters = 0
-    digits_in_first_character = 0
 
     is_int = False
 
-    total_length_minus_one = len(str1) - 1
-
-    """
-    这边其实有一个讨巧的办法：
-    try:
-        int character_input
-    except:
-        print(character_input)
-    """
+    digits_in_string = 0
+    total_length = len(string)
 
     for character_input in remain_characters:
         for character in "1234567890":
             if character_input== character:
                 # print("j, k: %s, %s" % (j, k))
-                digits_in_remain_characters += 1
+                digits_in_string += 1
                 break
 
-    character_input = first_character_str1
     for character in "1234567890-":
-        if character_input == character:
-            digits_in_first_character += 1
+        if first_character == character:
+            digits_in_string += 1
             break
 
-    # print(total_length_minus_two)
-    if digits_in_remain_characters == total_length_minus_one and digits_in_first_character == 1:
+    if digits_in_string == total_length:
         is_int = True
 
-    if is_int:
-        return True
-    else:
-        # print("Error: Nor int number:")
+    if not is_int:
         tk.messagebox.showerror(title="Error", message="请输入整数!")
-        return False
+
+    return is_int
 
 
 
-def random_number():
+
+
+def generate_random_number():
     if check_empty():
         return
 
@@ -133,9 +78,6 @@ def random_number():
 
     if exceed_len_max(get1, get2):
         return
-
-    # if not is_number(get1, get2):
-    #     return
 
     if (not is_number(get1)) or (not is_number(get2)):
         return
@@ -150,9 +92,36 @@ def random_number():
     if num1 > num2:
         num1, num2 = num2, num1
 
-    result = randint(num1, num2)
 
-    var.set(f"{result}")
+
+    random_number = randint(num1, num2)
+
+
+    # legal_exception = None
+    exception_input_list = []
+
+
+
+    equal_to_exception = False
+    equal_number = None
+    for tmp_str in exception_input_list:
+        if tmp_str == random_number:
+            equal_to_exception = True
+            equal_number = tmp_str
+            break
+
+    if equal_to_exception and equal_number == num1 and num1 == num2:
+        print("No output")
+        return
+    elif equal_to_exception:
+        generate_random_number()
+
+    var.set(f"{random_number}")
+
+def clean_input_box():
+    entry1.delete("0", tk.END)
+    entry2.delete("0", tk.END)
+    entry3.delete("0", tk.END)
 
 
 root = tk.Tk()
@@ -169,30 +138,46 @@ var = tk.StringVar()
 label100 = tk.Label(root, textvariable=var, font=("Arial", 50), height=3)
 label100.grid(row=0, column=0, columnspan=2)
 
-label1 = tk.Label(root, text="请输入开始数字(包含):", font=("Arial", 30), width=20, height=1)
+label1 = tk.Label(root, text="请输入开始数字(包含):", font=("Arial", 30), width=26, height=1)
 label1.grid(row=1, column=0)
 
-entry1 = tk.Entry(root, font=45, width=100)
+entry1 = tk.Entry(root, font=45, width=80)
 entry1.grid(row=1, column=1)
 
-label101_text1 = "使用说明:\n\n将随机在输入的两个数之间(左闭右闭)寻找一个随机数\n\n\n"
-label101_text2 = "Attention:\n\n请输入一个整数( -10**32 <= n <= 10**32)."
-label101 = tk.Label(root, text=label101_text1 + label101_text2, font=45, width=50, height=10)
-label101.grid(row=2, column=0, columnspan=2)
 
-label2 = tk.Label(root, text="请输入结束数字(包含):", font=("Arial", 30), width=20, height=1)
-label2.grid(row=3, column=0)
+label2 = tk.Label(root, text="请输入结束数字(包含):", font=("Arial", 30), width=26, height=1)
+label2.grid(row=2, column=0)
 
-entry2 = tk.Entry(root, font=45, width=100)
-entry2.grid(row=3, column=1)
+entry2 = tk.Entry(root, font=45, width=80)
+entry2.grid(row=2, column=1)
 
-label102 = tk.Label(root, text="祝你好运\tGood Luck", font=45, width=50, height=10)
-label102.grid(row=4, column=0, columnspan=2)
 
-button1 = tk.Button(root, text="Start", font=30, width=50, height=2, command=random_number)
-button1.grid(row=5, column=0, columnspan=2)
+label3 = tk.Label(root, text="请输入需要排除的数字(可不输入):", font=("Arial", 30), width=26, height=1)
+label3.grid(row=3, column=0)
 
-var.set("Please enter the two values")
+entry3 = tk.Entry(root, font=45, width=80)
+entry3.grid(row=3, column=1)
+
+
+label101_text1 = "\n使用说明:\n\n将随机在输入的两个数之间(左闭右闭)寻找一个随机数\n\n"
+label101_text2 = "生成随机数将不会是被排除的数(支持排除多个数, 用逗号或分号隔开)\n\n\n"
+label101_text3 = "Attention:\n\n请输入一个整数( -10^32 <= n <= 10^32)."
+label101_text = label101_text1 + label101_text2 + label101_text3
+label101 = tk.Label(root, text=label101_text, font=45, width=100, height=13)
+label101.grid(row=4, column=0, columnspan=2)
+
+
+label102 = tk.Label(root, text="祝你好运\tGood Luck", font=("Arial", 20), width=50, height=2)
+label102.grid(row=5, column=0, columnspan=2)
+
+
+button1 = tk.Button(root, bg="cyan", text="Start", font=30, width=50, height=2, command=generate_random_number)
+button1.grid(row=6, column=0)
+
+button2 = tk.Button(root, bg="cyan", text="一键清除", font=30, width=50, height=2, command=clean_input_box)
+button2.grid(row=6, column=1)
+
+var.set("Please enter the two values🤓")
 
 
 """
